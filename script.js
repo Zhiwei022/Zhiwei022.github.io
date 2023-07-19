49136618e9d1b890baf105ad8a69b392291d891a
Zhiwei022 <wayne64081143@gmail.com>
2023-07-20 01:19:16 +0800

    initial update
    complete rough web content

---
+++ script.js
@@ -0,0 +1,35 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId)
+    const nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+if (document.getElementById('my-work-link')) {
+    document.getElementById('my-work-link').addEventListener('click', () => {
+      document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+    })
+  }
+
+
